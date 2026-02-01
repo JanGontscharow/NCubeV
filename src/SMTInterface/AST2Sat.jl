254ba45dd0eead694d278ec9abbf8dcb0f2590d9
JanGontscharow <jan.gontscharow@gmail.com>
2026-02-01 17:02:22 +0100

    added AST2Sat

---
+++ src/SMTInterface/AST2Sat.jl
@@ -0,0 +1,241 @@
+using Satisfiability
+Sat = Satisfiability
+
+# TODO(steuber): Floating Point Correctness?
+function ast2smt(f :: CompositeFormula, variables, additional, smt_cache=Dict())
+	if haskey(smt_cache, f)
+		return smt_cache[f]
+	end
+	arguments = map(x -> ast2smt(x, variables, additional, smt_cache), f.args)
+	res = @match f.connective begin
+		Not => Sat.not(arguments[1])
+		And => Sat.and(arguments...)
+		Or => Sat.or(arguments...)
+		Implies => Sat.implies(arguments[1],arguments[2])
+		ITE => Sat.ite(arguments[1],arguments[2],arguments[3])
+	end
+
+	smt_cache[f] = res
+	return res
+end
+function ast2smt(f :: TrueAtom, variables, additional, smt_cache)
+	@satvariable(t, Bool)
+	if haskey(smt_cache, f)
+		return smt_cache[f]
+	end
+    res = Sat.__wrap_const(true)
+	smt_cache[f] = res
+	return res
+end
+function ast2smt(f :: FalseAtom, variables, additional, smt_cache)
+	@satvariable(t, Bool)
+	if haskey(smt_cache, f)
+		return smt_cache[f]
+	end
+    res = Sat.__wrap_const(false)
+	smt_cache[f] = res
+	return res
+end
+"""
+	ast2smt(f::LinearConstraint, variables, additional, smt_cache)
+
+Encode a linear (or weakly linear) constraint `A*x [</<=] b` as a Sat
+comparison. Coefficients and bias are rationalized for stability.
+"""
+#TODO(steuber): FLOAT INCORRECTNESS
+function ast2smt(f :: LinearConstraint, variables, additional, smt_cache=Dict())
+	
+	if haskey(smt_cache, f)
+		return smt_cache[f]
+	end
+	
+	coeff = map(c -> ast2smt(TermNumber(c), variables, additional, smt_cache), f.coefficients)
+	bias = ast2smt(TermNumber(f.bias), variables, additional, smt_cache)
+	n = length(coeff) # variables may have more entries than coefficients (input constraints)
+
+
+	# not working, due to incorrect behavior of multiplication with 0.0
+	lincomb = sum(coeff .* variables[1:n])
+	#lincomb = 0.0
+	#for i in 1:n
+	#	if (coeff[i] != 0.0) && (variables[i].value != 0.0)
+	#		lincomb += coeff[i] * variables[i]
+	#	end
+	#end
+
+	res = f.equality ? lincomb ≤ bias : lincomb < bias
+	
+	smt_cache[f] = res
+	return res
+end
+
+"""
+	ast2smt(t::LinearTerm, variables, additional, smt_cache)
+
+Lower a linear term into a Sat arithmetic expression.
+"""
+function ast2smt(t :: LinearTerm, variables, additional, smt_cache)
+	
+	if haskey(smt_cache, t)
+		return smt_cache[t]
+	end
+
+	#@info "linear-term: $t"
+
+	coeff = map(c -> ast2smt(TermNumber(c), variables, additional, smt_cache), t.coefficients)
+	for c in coeff
+		if isa(c, Sat.NumericExpr)
+			c = c.value
+		end
+	end
+	
+	#@info "coeff: $coeff"
+	
+	bias = ast2smt(TermNumber(t.bias), variables, additional, smt_cache)
+	n = length(coeff) # variables may have more entries than coefficients (input constraints)
+	
+	#@info "vec: $(coeff .* variables[1:n])"
+
+	# not working, due to incorrect behavior of multiplication with 0.0
+	lincomb = sum(coeff .* variables[1:n])
+	#lincomb = 0.0
+	#for i in 1:n
+	#	if coeff[i] != 0.0
+	#		lincomb += coeff[i] * variables[i]
+	#	end
+	#end
+	#@info "lincomb: $lincomb"
+
+	res = lincomb + bias
+
+	#@info "res: $res"
+
+	smt_cache[t] = res
+
+
+	return res
+end
+
+"""
+	ast2smt(f::ApproxNode, ...)
+
+Forward translation to the underlying formula carried by an approximation node.
+"""
+function ast2smt(f :: ApproxNode, variables, additional, smt_cache)
+	if haskey(smt_cache, f)
+		return smt_cache[f]
+	end
+	res = ast2smt(f.formula, variables, additional, smt_cache)
+	smt_cache[f] = res
+	return res
+end
+"""
+	ast2smt(f::Atom, ...)
+
+Translate atomic comparisons by recursively lowering both sides and applying
+the respective Sat comparator.
+"""
+function ast2smt(f :: Atom, variables, additional, smt_cache=Dict())
+	if haskey(smt_cache, f)
+		return smt_cache[f]
+	end
+	termLeft = ast2smt(f.left, variables, additional, smt_cache)
+	termRight = ast2smt(f.right, variables, additional, smt_cache)
+	res = @match f.comparator begin
+		Less => termLeft < termRight
+		LessEq => termLeft <= termRight
+		Greater => termLeft > termRight
+		GreaterEq => termLeft >= termRight
+		Eq => termLeft == termRight
+		Neq => Sat.distinct(termLeft, termRight)
+	end
+	smt_cache[f] = res
+	return res
+end
+
+
+function smt_pow(base, exp, variables=[], additional=[], smt_cache=Dict())
+	@assert isa(exp, TermNumber) "Exponent must be a TermNumber."
+	@assert isa(base, TermNumber) || isa(base, Variable) "Base must be a TermNumber, Variable."
+	
+	num = exp.value.num
+	den = exp.value.den
+
+	if den == 1
+		if isa(base, TermNumber)
+			return ast2smt(base^exp, variables, additional, smt_cache)
+		else
+			var = ast2smt(base, variables, additional, smt_cache)
+			if num > 0
+				# xⁿ = x ⋅ x ⋯ x
+				return foldl(*, fill(var, num))
+			elseif num < 0
+				# x⁻ⁿ = 1 / (x ⋅ x ⋯ x)
+				return 1.0 / foldl(*, fill(var, -num))
+			else
+				return 1.0
+			end		
+		end
+	else
+		@assert false "Non-integer exponents not supported in SMT backend yet."
+		# TODO(steuber): Implement roots again (but probably hard for SMT solver anyway...)
+	end
+end
+
+"""
+	ast2smt(f::CompositeTerm, ...)
+
+Handle arithmetic combinations Add/Sub/Mul/Div/Pow/Neg. For non-integer
+exponents, guards restrict the domain to non-negative and fall back to 0 otherwise.
+"""
+function ast2smt(f :: CompositeTerm, variables, additional, smt_cache)
+	if haskey(smt_cache, f)
+		return smt_cache[f]
+	end
+	if f.operation ≠ Pow
+		arguments = map(x -> ast2smt(x, variables, additional, smt_cache), f.args)
+		res = @match f.operation begin
+			Add => +(arguments...)
+			Sub => -(arguments...)
+			Mul => *(arguments...)
+			Div => /(arguments...)
+			Neg => return -arguments[1]
+		end
+	else
+		@assert length(f.args) == 2 "Pow operation requires exactly two arguments."
+		res = smt_pow(f.args..., variables, additional, smt_cache)
+	end
+	
+	smt_cache[f] = res
+	return res
+end
+function ast2smt(v :: Variable, variables, additional, smt_cache)
+	return variables[v.position]
+end
+function ast2smt(n::TermNumber, variables, additional, smt_cache)
+	x = Float64(n.value)
+	x_str = string(x)
+
+	if !contains(x_str, "e")
+		return x
+	else
+		@warn "$(x) contains scientific notation. adding shield variable."
+		@satvariable(t_shield, Real)
+		push!(additional, t_shield == 1.0)
+
+		parts = split(x_str, 'e')
+		coeff = parse(Float64, parts[1])
+		exponent = parse(Int, parts[2])
+
+		@assert exponent < 0 "Only negative exponents are supported for shield variables."
+		
+		divisor = 10.0^(-exponent)
+
+		@assert !contains(string(divisor), "e") "Shield variable divisor cannot be in scientific notation."
+
+		return (coeff / (divisor * t_shield))
+	end
+	
+end
+
+
